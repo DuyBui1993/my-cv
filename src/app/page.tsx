ffeebdf67b40b05f36cd1b649f1ab5c8fa3a9abc
Duy Bui <duy.bui@doltech.vn>
2024-09-30 20:36:44 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <p className="text-4xl">DUY SITE HERE</p>
+        <p className="text-4xl">DUY SITE HERE 1111</p>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
